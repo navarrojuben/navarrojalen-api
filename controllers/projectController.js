140b8502ecb31bb0b2c93eb863098d790cb3d8cb
Juben Navarro <juben.lnavarro@gmail.com>
2025-05-12 17:53:52 +0800

    Initial Commit

---
+++ controllers/projectController.js
@@ -0,0 +1,26 @@
+const Project = require('../models/Project');
+
+// GET all
+exports.getProjects = async (req, res) => {
+  const projects = await Project.find();
+  res.json(projects);
+};
+
+// POST new
+exports.createProject = async (req, res) => {
+  const project = new Project(req.body);
+  const saved = await project.save();
+  res.status(201).json(saved);
+};
+
+// PUT update
+exports.updateProject = async (req, res) => {
+  const project = await Project.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(project);
+};
+
+// DELETE
+exports.deleteProject = async (req, res) => {
+  await Project.findByIdAndDelete(req.params.id);
+  res.json({ message: 'Deleted successfully' });
+};
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/projects', require('./routes/projectRoutes'));
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
